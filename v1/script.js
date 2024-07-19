70a3094db612765604378965a984fad34bdee3a0
Juli S. <51840045+itsOwlbit@users.noreply.github.com>
2024-07-18 20:18:41 -0700

    Basic grid with test numbers.
    Grid layout with some numbers.  Need to fix issue with grid when changing input to disabled for starting numbers so it is not editable.  Experiment with maybe another layout design.

---
+++ v1/script.js
@@ -0,0 +1,33 @@
+const gridContainer = document.getElementById('grid');
+
+const startValues = [[9,0,0,3,4,0,0,7,0],
+                     [8,5,0,7,0,0,3,4,9],
+                     [0,7,0,8,9,1,2,0,0],
+                     [0,6,8,0,0,0,4,0,0],
+                     [3,0,0,6,8,7,0,1,5],
+                     [0,0,0,0,5,4,0,0,3],
+                     [0,0,0,0,0,8,0,0,0],
+                     [0,0,4,1,0,2,5,0,0],
+                     [6,0,5,0,7,9,1,0,2]];
+
+// create 9x9 sudoku game board grid
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridContainer.appendChild(cell);
+
+        const input = document.createElement('input');
+        input.min = 1;
+        input.max = 1;
+        input.classList.add('input-cell');
+
+        if (startValues[i][j] !== 0) {
+            input.value = startValues[i][j];
+            // input.disabled = true;
+        }
+     
+        cell.appendChild(input);
+        // console.log(i + ' ' + j);
+    }
+}
